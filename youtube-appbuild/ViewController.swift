39a828c19e071ddfe2e6917e1a33322ee3220779
baangs <aey.baangs@gmail.com>
2020-08-10 23:30:58 -0700

    data source and delegate calls added

--- youtube-appbuild/ViewController.swift
+++ youtube-appbuild/ViewController.swift
@@ -5,19 +5,44 @@
 //  Created by RD on 7/11/20.
 //  Copyright © 2020 Reggie Daniels. All rights reserved.
 //
+//create new instance of model class
+ // Do any additional setup after loading the view.
 
 import UIKit
 
-class ViewController: UIViewController {
-    //create new instance of model class
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+ 
+   
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     var model = Model()
+    var videos = [Video]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
+        //set itself as datasource and delegate
+        tableView.dataSource = self
+        tableView.delegate = self
         
         model.getVideos()
         
+    }
+    
+    //mark - tableview methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+     }
+     
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         <#code#>
+     }
+     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
     }
 
 
